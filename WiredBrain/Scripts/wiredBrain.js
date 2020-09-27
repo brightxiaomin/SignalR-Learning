95865ee808fcc269a0009f7d239ccca8b040b815
Min Xiao <mxiao3@gmail.com>
2020-09-26 23:04:39 -0500

    add client

---
+++ WiredBrain/Scripts/wiredBrain.js
@@ -0,0 +1,48 @@
+﻿setupConnection = (hubProxy) => {
+    hubProxy.client.receiveOrderUpate = (updateObject) => {
+        const statusDiv = document.getElementById("status");
+        statusDiv.innerHTML = `Order: ${updateObject.OrderId} : ${updateObject.Update}`;
+    };
+
+    //hubProxy.client.newOrder = (order) => {
+    //    const statusDiv = document.getElementById("status");
+    //    statusDiv.innerHTML = `Somebody ordered an ${order.Product}`;
+    //};
+
+    //hubProxy.logging = true;
+
+    hubProxy.client.finished = (order) => {
+        console.log(`Finished coffee order ${order}`);
+    };
+
+};
+
+
+$.connection.hub.logging = true;
+$(() => {
+    var hubProxy = $.connection.coffeeHub;
+    setupConnection(hubProxy);
+    $.connection.hub.logging = true;
+    $.connection.hub.start();
+
+    document.getElementById("submit").addEventListener("click",
+        e => {
+            e.preventDefault();
+            var statusDiv = document.getElementById("status");
+            statusDiv.innerHTML = "Submitting order...";
+
+            const product = document.getElementById("product").value;
+            const size = document.getElementById("size").value;
+
+            fetch("api/coffee",
+                {
+                    method: "POST",
+                    body: JSON.stringify({ product, size }),
+                    headers: { 'content-type': 'application/json' }
+                })
+                .then(res => res.text())
+                .then(id => hubProxy.server.getUpdateForOrder({ id, product, size }));
+
+        }
+    );
+});
