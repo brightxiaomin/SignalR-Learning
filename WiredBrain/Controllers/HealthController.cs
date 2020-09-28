c1a94977e7b66c8e7fcacadc3c163de09c28335a
Min Xiao <mxiao3@gmail.com>
2020-09-27 21:22:42 -0500

    add a health check endpoint

---
+++ WiredBrain/Controllers/HealthController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+
+namespace WiredBrain.Controllers
+{
+    public class HealthController : ApiController
+    {
+        [HttpGet]
+        public async Task<IHttpActionResult> Check()
+        {
+            // Add logic here to check dependencies
+
+            HubConnection hubConnection = new HubConnection("http://localhost:61055/");
+            //var hubproxy = hubConnection.CreateHubProxy("CoffeeHub");
+            try
+            {
+                await hubConnection.Start();
+                // return Ok();
+                return Json("Healthy");
+
+            }
+            catch
+            {
+                return InternalServerError();
+            }
+
+            finally
+            {
+                 hubConnection?.Dispose();
+            }
+            //if (2==2)
+            //{
+            //    string test = "Healthy";
+            //    return Ok(test);
+            //}
+
+            //return InternalServerError(); // Or whatever other HTTP status code is appropriate
+        }
+    }
+}
--- WiredBrain/Scripts/wiredBrain.js
+++ WiredBrain/Scripts/wiredBrain.js
@@ -1,80 +1,52 @@
-﻿//setupConnection = (hubProxy) => {
-//    hubProxy.client.receiveOrderUpate = (updateObject) => {
-//        const statusDiv = document.getElementById("status");
-//        statusDiv.innerHTML = `Order: ${updateObject.OrderId} : ${updateObject.Update}`;
-//    };
-
-//    hubProxy.client.newOrder = (order) => {
-//        const statusDiv = document.getElementById("status");
-//        statusDiv.innerHTML = `Somebody ordered an ${order.Product}`;
-//    };
-
-//    //hubProxy.logging = true;
-
-//    hubProxy.client.finished = (order) => {
-//        console.log(`Finished coffee order ${order}`);
-//    };
-
-//};
-
-setupConnection = function (hubProxy) {
-    hubProxy.client.receiveOrderUpate = function (updateObject) {
+﻿setupConnection = (hubProxy) => {
+    hubProxy.client.receiveOrderUpate = (updateObject) => {
         const statusDiv = document.getElementById("status");
-        //statusDiv.innerHTML = `Order: ${updateObject.OrderId} : ${updateObject.Update}`;
+        statusDiv.innerHTML = `Order: ${updateObject.OrderId} : ${updateObject.Update}`;
     };
 
-    hubProxy.client.newOrder = function (order) {
+    hubProxy.client.newOrder = (order) => {
         const statusDiv = document.getElementById("status");
-        //statusDiv.innerHTML = `Somebody ordered an ${order.Product}`;
+        statusDiv.innerHTML = `Somebody ordered an ${order.Product}`;
     };
 
     //hubProxy.logging = true;
 
-    hubProxy.client.finished = function (order) {
-        //console.log(`Finished coffee order ${order}`);
-        console.log("finished");
+    hubProxy.client.finished = (order) => {
+        console.log(`Finished coffee order ${order}`);
     };
+
 };
+
+//setupConnection = function (hubProxy) {
+//    hubProxy.client.receiveOrderUpate = function (updateObject) {
+//        const statusDiv = document.getElementById("status");
+//        //statusDiv.innerHTML = `Order: ${updateObject.OrderId} : ${updateObject.Update}`;
+//    };
+
+//    hubProxy.client.newOrder = function (order) {
+//        const statusDiv = document.getElementById("status");
+//        //statusDiv.innerHTML = `Somebody ordered an ${order.Product}`;
+//    };
+
+//    //hubProxy.logging = true;
+
+//    hubProxy.client.finished = function (order) {
+//        //console.log(`Finished coffee order ${order}`);
+//        console.log("finished");
+//    };
+//};
 
 
 // $.connection.hub.logging = true;
 
-$(document).ready(function () {
-    var hubProxy = $.connection.coffeeHub;
-    setupConnection(hubProxy);
-    $.connection.hub.logging = true;
-    $.connection.hub.start();
-
-    document.getElementById("submit").addEventListener("click",
-        function (e) {
-            e.preventDefault();
-            var statusDiv = document.getElementById("status");
-            statusDiv.innerHTML = "Submitting order...";
-
-            const product = document.getElementById("product").value;
-            const size = document.getElementById("size").value;
-
-            fetch("api/coffee",
-                {
-                    method: "POST",
-                    body: JSON.stringify({ 'product': product, 'size': size }),
-                    headers: { 'content-type': 'application/json' }
-                });
-                //.then(res => res.text())
-                //.then(id => hubProxy.server.getUpdateForOrder({ id, product, size }));
-
-        }
-    );
-});
-
-//$(() => {
+//$(document).ready(function () {
 //    var hubProxy = $.connection.coffeeHub;
 //    setupConnection(hubProxy);
 //    $.connection.hub.logging = true;
 //    $.connection.hub.start();
 
 //    document.getElementById("submit").addEventListener("click",
-//        e => {
+//        function (e) {
 //            e.preventDefault();
 //            var statusDiv = document.getElementById("status");
 //            statusDiv.innerHTML = "Submitting order...";
@@ -85,12 +57,40 @@ $(document).ready(function () {
 //            fetch("api/coffee",
 //                {
 //                    method: "POST",
-//                    body: JSON.stringify({ product, size }),
+//                    body: JSON.stringify({ 'product': product, 'size': size }),
 //                    headers: { 'content-type': 'application/json' }
-//                })
-//                .then(res => res.text())
-//                .then(id => hubProxy.server.getUpdateForOrder({ id, product, size }));
+//                });
+//                //.then(res => res.text())
+//                //.then(id => hubProxy.server.getUpdateForOrder({ id, product, size }));
 
 //        }
 //    );
 //});
+
+$(() => {
+    var hubProxy = $.connection.coffeeHub;
+    setupConnection(hubProxy);
+    $.connection.hub.logging = true;
+    $.connection.hub.start();
+
+    document.getElementById("submit").addEventListener("click",
+        e => {
+            e.preventDefault();
+            var statusDiv = document.getElementById("status");
+            statusDiv.innerHTML = "Submitting order...";
+
+            const product = document.getElementById("product").value;
+            const size = document.getElementById("size").value;
+
+            fetch("api/coffee",
+                {
+                    method: "POST",
+                    body: JSON.stringify({ product, size }),
+                    headers: { 'content-type': 'application/json' }
+                })
+                .then(res => res.text())
+                .then(id => hubProxy.server.getUpdateForOrder({ id, product, size }));
+
+        }
+    );
+});
